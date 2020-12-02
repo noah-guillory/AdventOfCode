321a703a5ae0180475ef6689b2cc4567dde59b39
Noah Guillory <noah.guillory@storable.com>
2020-12-02 13:15:15 -0600

    Final cleanup

--- AdventOfCode/2019/Program.fs
+++ AdventOfCode/2019/Program.fs
@@ -1,4 +1,4 @@
-﻿namespace AdventOfCode.Y2020
+﻿namespace AdventOfCode.Y2019
 
 open AdventOfCode
 open AdventOfCode.Common
@@ -7,12 +7,12 @@ open AdventOfCode.Benchmarking
 module Program =
     let getSolver day part printAnswer =
         let run (solver : Day<_, _, _>) =
-            Runner.run printAnswer 2020 day part solver
+            Runner.run printAnswer 2019 day part solver
         match day with
-        | 1  -> run Year2020Day01.solver | 2  -> run Year2020Day02.solver
-        | day -> fun _ -> printfn "Invalid Day: %i (Year %i)" day 2020
+        | 1  -> run Year2019Day01.solver
+        | day -> fun _ -> printfn "Invalid Day: %i (Year %i)" day 2019
 
-    type Bench2020() =
+    type Bench2019() =
         inherit Bench() with
             override _.GetSolverFunc day part () =
                 getSolver day part false ()
@@ -22,7 +22,7 @@ module Program =
         let runPart day part = getSolver day part true ()
         let runDay day = for part in 1..2 do runPart day part
         match argv.[0] with
-            | "BENCH" -> Benchmarking.runBenchmarks<Bench2020>()
+            | "BENCH" -> Benchmarking.runBenchmarks<Bench2019>()
             | "ALL" -> for day in 1..2 do runDay day
             | x ->
                 let parts = x.Split('.') |> Array.map int
